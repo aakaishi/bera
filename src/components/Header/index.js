46e3b997664f51d61c24e7bce51edd46172fb38b
aakaishi <adrianoakaishi@gmail.com>
2023-11-12 18:28:44 +1100

    Add Main component. Start Form component.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,14 @@ import Menu from '../Menu';
 const Header = () => {
   return (
     <header className='header'>
-      <Menu />
+      <section className='top'>
+        <p>AUSTRALIA $AU</p>
+      </section>
+      <section className='middle'>
+        <Menu />
+      </section>
+      <section className='bottom'>
+      </section>
     </header>
   );
 }
---
+++ src/components/Main/index.js
@@ -0,0 +1,16 @@
+import './Main.css';
+
+const Main = () => {
+  return (
+    <main className='main'>
+      <section className='title'>
+        <h1>Content title</h1>
+      </section>  
+      <section className='content'>
+        <h2>Content will be here.</h2>
+      </section>  
+    </main>
+  )
+}
+
+export default Main;
